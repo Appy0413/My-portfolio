a64a22b2b807f850b10c30d16ca4c7102ae46b55
Aprajita Singh <aprajita0496@gmail.com>
2024-07-12 16:30:43 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    
+    navLinks.forEach(link => {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        
+        window.scrollTo({
+          top: targetSection.offsetTop,
+          behavior: 'smooth'
+        });
+      });
+    });
+    
+    const contactForm = document.getElementById('contact-form');
+    
+    contactForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      alert('Message sent!');
+      contactForm.reset();
+    });
+  });
+  
